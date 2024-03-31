8150351a5644408a4b31c6c998dc287abdaa016f
OleksandraMakarets <DELETED-1702562713224>
2024-03-31 20:08:17 +0000

    Ensure block fits into the grid before moving

--- game.py
+++ game.py
@@ -19,19 +19,19 @@ class Game:
     
     def move_left(self):
 		self.current_block.move(0, -1)
-        if self.block_inside() == False or self.block_fits() == False:
+        if self.block_inside() == False or self.block_fits() == False or self.block_fits() == False:
 			self.current_block.move(0, 1)
 		
 
 	def move_right(self):
 		self.current_block.move(0, 1)
-        if self.block_inside() == False or self.block_fits() == False:
+        if self.block_inside() == False or self.block_fits() == False or self.block_fits() == False:
 			self.current_block.move(0, -1)
 		
 
 	def move_down(self):
 		self.current_block.move(1, 0)
-        if self.block_inside() == False or self.block_fits() == False:
+        if self.block_inside() == False or self.block_fits() == False or self.block_fits() == False:
 			self.current_block.move(-1, 0)
             self.lock_block()
 
