ab05228d53a37b15d4585c47c7671155e6758e4c
OleksandraMakarets <DELETED-1702562713224>
2024-03-31 16:55:24 +0000

    Create pygame window

--- run.py
+++ run.py
@@ -1,2 +1,21 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+dark_blue(44, 44, 127)
+
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Tetris")
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #Drawing
+    screen.fill(darl_blue)
+    pygame.display.update()
+    clock.tick(60)
 
