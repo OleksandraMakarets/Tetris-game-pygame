d9cd37c7fba71e61f8e38ffc8d5dab70e3b7ad5b
OleksandraMakarets <DELETED-1702562713224>
2024-03-31 19:11:01 +0000

    Add get_random_block method in Game class

--- game.py
+++ game.py
@@ -7,3 +7,8 @@ class Game:
 	def __init__(self):
 		self.grid = Grid()
 		self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+
+    def get_random_block(self):
+		block = random.choice(self.blocks)
+		self.blocks.remove(block)
+		return block
