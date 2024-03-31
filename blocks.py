efe27de70bd3ac5a567ec478c06cadea010e561f
OleksandraMakarets <DELETED-1702562713224>
2024-03-31 18:49:05 +0000

    Create TBlock class inheriting from Block class to represent a J-shaped block in the game

--- blocks.py
+++ blocks.py
@@ -47,3 +47,13 @@ class SBlock(Block):
             2: [Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)],
             3: [Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1)]
         }
+
+class TBlock(Block):
+    def __init__(self):
+        super().__init__(id = 6)
+        self.cells = {
+            0: [Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
+        }
